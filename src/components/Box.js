ff1561362545fe4b958ba08e69cd5d9418b2fabe
Rowan Gordon <rowan@rowangordon.co.uk>
2021-11-10 22:17:06 +0100

    styling almost complete

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import Img from "../images/citadel144.png"
 
 const Section = styled.section`
   display: flex;
@@ -43,8 +44,13 @@ const Section = styled.section`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    padding: 150px 0 180px 0;
+    /* padding: 150px 0 180px 0; */
+    height: 360px;
     width: 600px;
+    padding-bottom: 1rem;
+    h3 {
+      color: #fff;
+    }
     p {
       margin: 0 0 0.5rem 0;
       color: #fff;
@@ -71,12 +77,70 @@ const Section = styled.section`
         cursor: pointer;
       }
     }
+    .white-container {
+      background-color: #fff;
+      padding: 0.5rem 1rem;
+      display: flex;
+      width: 420px;
+      align-items: center;
+      justify-content: space-between;
+      border-radius: 3px;
+      img {
+        width: 200px;
+        margin: 1rem;
+      }
+      .results-text {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 50%;
+        max-width: 200px;
+        p {
+          color: #000;
+        }
+        .pink {
+          color: pink;
+        }
+        .red {
+          color: red;
+        }
+        .green {
+          color: green;
+        }
+        .orange {
+          color: orange;
+        }
+        .row-space {
+          display: flex;
+          justify-content: space-between;
+          width: 150px;
+        }
+        .row-center {
+          display: flex;
+          justify-content: center;
+          width: 150px;
+          margin-bottom: -0.7rem;
+          margin-top: 0.4rem;
+        }
+      }
+    }
   }
 `
+// const PopUp = styled.section`
+//   position: absolute;
+//   height: inherit;
+//   width: 600px;
+//   background-color: red;
+// `
 
 const Box = () => {
   const [tab, setTab] = React.useState(1)
   const [tokenID, setTokenID] = React.useState(0)
+  const [popup1, setPopup1] = React.useState(false)
+  const [popup2, setPopup2] = React.useState(false)
+  const [popup3, setPopup3] = React.useState(false)
+  const [popup4, setPopup4] = React.useState(false)
 
   const colors = {
     1: "#171717",
@@ -97,16 +161,19 @@ const Box = () => {
   }
 
   const handleChange = event => {
-    const token = event.target.value
-    setTokenID(token)
+    setTokenID(event.target.value)
   }
 
   return (
     <Section style={{ backgroundColor: colors[tab] }}>
+      {/* <PopUp>hello</PopUp> */}
       <div className="tabs">
         <div
           onClick={() => {
             setTab(1)
+            setPopup2(false)
+            setPopup3(false)
+            setPopup4(false)
           }}
         >
           IDENTITY
@@ -114,6 +181,9 @@ const Box = () => {
         <div
           onClick={() => {
             setTab(2)
+            setPopup1(false)
+            setPopup3(false)
+            setPopup4(false)
           }}
         >
           VAULT
@@ -121,6 +191,9 @@ const Box = () => {
         <div
           onClick={() => {
             setTab(3)
+            setPopup1(false)
+            setPopup2(false)
+            setPopup4(false)
           }}
         >
           CACHE
@@ -128,23 +201,77 @@ const Box = () => {
         <div
           onClick={() => {
             setTab(4)
+            setPopup1(false)
+            setPopup2(false)
+            setPopup3(false)
           }}
         >
-          ?
+          LAND
         </div>
       </div>
-      {tab === 1 && (
+      {tab === 1 && popup1 === false && (
         <div className="main-section">
-          <p>INPUT TOKENID...</p>
+          <p>INPUT IDENTITY TOKENID...</p>
           <form onSubmit={handleSubmit}>
             <input type="number" name="token" onChange={handleChange} />
-            <button type="submit">SUBMIT</button>
+            <button
+              type="submit"
+              onClick={() => {
+                setPopup1(true)
+              }}
+            >
+              SUBMIT
+            </button>
           </form>
         </div>
       )}
+      {tab === 1 && popup1 && (
+        <div className="main-section">
+          <h3>IDENTITY #144</h3>
+          <div className="white-container">
+            <img src={Img} alt="image" />
+            <div className="results-text">
+              <div className="row-space">
+                <p className="pink">RARITY</p>
+                <p>1794</p>
+              </div>
+              <div className="row-space">
+                <p className="red">FLOOR</p>
+                <p>19E/$100,000</p>
+              </div>
+              <div className="row-space">
+                <p className="green">HIGH</p>
+                <p>70E/$300,000</p>
+              </div>
+              <div className="row-center">
+                <p className="orange">MARKET PRICE</p>
+              </div>
+              <div className="row-center">
+                <p>22E/$140,000</p>
+              </div>
+              <div className="row-center" style={{ marginTop: "1rem" }}>
+                <p
+                  style={{
+                    border: "1px solid #000",
+                    padding: "2px 8px",
+                    cursor: "pointer",
+                    borderRadius: "3px",
+                  }}
+                  onClick={() => {
+                    setPopup1(false)
+                  }}
+                >
+                  RESET
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
       {tab === 2 && (
         <div className="main-section">
-          <p>INPUT TOKENID...</p>
+          <p>INPUT VAULT TOKENID...</p>
           <form onSubmit={handleSubmit}>
             <input type="number" name="token" onChange={handleChange} />
             <button type="submit">SUBMIT</button>
@@ -153,7 +280,7 @@ const Box = () => {
       )}
       {tab === 3 && (
         <div className="main-section">
-          <p>INPUT TOKENID...</p>
+          <p>INPUT CACHE TOKENID...</p>
           <form onSubmit={handleSubmit}>
             <input type="number" name="token" onChange={handleChange} />
             <button type="submit">SUBMIT</button>
@@ -162,7 +289,7 @@ const Box = () => {
       )}
       {tab === 4 && (
         <div className="main-section">
-          <p>INPUT TOKENID...</p>
+          <p>INPUT LAND TOKENID...</p>
           <form onSubmit={handleSubmit}>
             <input
               type="number"
